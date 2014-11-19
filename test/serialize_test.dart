73a130f9afde55d31c6ecd5cf3e109294ae32e13
AJ <ajravindiran@gmail.com>
2014-11-18 19:17:06 -0500

    more unit testing for serialization

--- test/serialize_test.dart
+++ test/serialize_test.dart
@@ -10,6 +10,12 @@ class Organization {
   String name;
   String motto;
   int yearFounded;
+  List<String> founders = ['Larry Page', 'Sergey Brin'];
+  Map<String, String> headquarters = {
+    'city': 'Mountain View',
+    'state': 'California',
+    'country': 'USA'
+  };
   
   int get companyAge {
     DateTime now = new DateTime.now();
@@ -24,9 +30,23 @@ void main() {
   
   unit.test('serialize organization object', () {
     Organization org = new Organization(1, 'Google', "Don't be evil!", 1998);
-    Map<String, Object> json = jmap.serialize(org);
-    
-    unit.expect('${JSON.encode(json)}', '{"id":1,"name":"Google","motto":"Don\'t be evil!","yearFounded":1998,"companyAge":16}');
+    Map<String, Object> data = jmap.serialize(org);
+
+    unit.expect(data.length, 7);
+    unit.expect(data['id'], 1);
+    unit.expect(data['name'], 'Google');
+    unit.expect(data['motto'], "Don't be evil!");
+    unit.expect(data['yearFounded'], 1998);
+    unit.expect(data['founders'], unit.isList);
+    unit.expect(data['headquarters'], unit.isMap);
+    unit.expect(data['companyAge'], 16);
+    unit.expect('${JSON.encode(data)}', 
+        '{"id":1,"name":"Google","motto":"Don\'t be evil!","yearFounded":1998,"founders":["Larry Page","Sergey Brin"],"headquarters":{"city":"Mountain View","state":"California","country":"USA"},"companyAge":16}'
+    );
+  });
+  
+  unit.test('scalar value should fail', () {
+    unit.expect(() => jmap.serialize("scalar value"), unit.throwsArgumentError);
   });
   
 }
