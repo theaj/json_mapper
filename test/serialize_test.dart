4bef7380ecc03437613cfc0d088de2ec5f26f8b8
AJ <ajravindiran@gmail.com>
2014-11-25 19:53:24 -0500

    test for deserialization

--- test/serialize_test.dart
+++ test/serialize_test.dart
@@ -1,7 +1,5 @@
 library serialize_test;
 
-import 'dart:convert';
-
 import 'package:unittest/unittest.dart' as unit;
 import 'package:json_mapper/json_mapper.dart' as jmap;
 
@@ -25,35 +23,39 @@ class Organization extends Entity {
     return now.year - yearFounded;
   }
   
-  Organization(int id, String this.name, 
+  Organization.from(int id, String this.name, 
       String this.motto, int this.yearFounded) {
     super.id = id;
   }
+  
+  Organization();
 }
 
-const String jsonString = '{"name":"Google","motto":"Don\'t be evil!","yearFounded":1998,"founders":["Larry Page","Sergey Brin"],"headquarters":{"city":"Mountain View","state":"California","country":"USA"},"companyAge":16,"id":1}';
+const String jsonString = '{"name":"Google","motto":"Don\'t be evil!","yearFounded":1998,"founders":["Larry Page","Sergey Brin"],"headquarters":{"city":"Mountain View","state":"California","country":"USA"},"id":1}';
 
 void main() {
   
   unit.test('serialize organization object', () {
-    Organization org = new Organization(1, 'Google', "Don't be evil!", 1998);
-    Map<String, Object> data = jmap.serialize(org);
-
-    unit.expect(data.length, 7);
-    unit.expect(data['id'], 1);
-    unit.expect(data['name'], 'Google');
-    unit.expect(data['motto'], "Don't be evil!");
-    unit.expect(data['yearFounded'], 1998);
-    unit.expect(data['founders'], unit.isList);
-    unit.expect(data['headquarters'], unit.isMap);
-    unit.expect(data['companyAge'], 16);
-    unit.expect('${JSON.encode(data)}', jsonString);
+    Organization org = new Organization.from(1, 'Google', "Don't be evil!", 1998);
+    unit.expect(jmap.serialize(org), jsonString);
   });
   
-  unit.test('scalar value should fail', () {
+  unit.test('serialize scalar value should fail', () {
     unit.expect(() => jmap.serialize("scalar value"), unit.throwsArgumentError);
   });
   
+  unit.test('deserialize json to object', () {
+    Organization org = jmap.deserialize(jsonString, Organization);
+    unit.expect(org.id, 1);
+    unit.expect(org.name, 'Google');
+    unit.expect(org.yearFounded, 1998);
+    unit.expect(org.founders, ['Larry Page', 'Sergey Brin']);
+    unit.expect(org.headquarters, {
+      'city': 'Mountain View',
+      'state': 'California',
+      'country': 'USA'
+    });
+  });
 }
 
 
